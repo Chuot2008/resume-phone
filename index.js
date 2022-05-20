9c13ff4f7b3c25a22f16a664e2b7272b51e4a168
Anh Truong <99332606+Chuot2008@users.noreply.github.com>
2022-05-20 22:29:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function aboutFunction() {
+    document.getElementById("aboutContainer").style.color = "red"
+    setTimeout(function() {
+        document.getElementById("aboutContainer").style.color = "black";
+    }, 800);
+}
+
+function eduFunction() {
+    document.getElementById("eduContainer").style.color = "red";
+
+    setTimeout(function() {
+        document.getElementById("eduContainer").style.color = "black";
+    }, 800);
+}
+
+function hobbyFunction() {
+    document.getElementById("hobbyContainer").style.color = "red"
+    setTimeout(function() {
+        document.getElementById("hobbyContainer").style.color = "black";
+    }, 800);
+}
+
+
+function scoreFunction() {
+    document.getElementById("scoresContainer").style.color = "red"
+    setTimeout(function() {
+        document.getElementById("scoresContainer").style.color = "black";
+    }, 800);
+}
+
+function socialFunction() {
+    document.getElementById("socialContainer").style.color = "red"
+    setTimeout(function() {
+        document.getElementById("socialContainer").style.color = "black";
+    }, 800);
+}
